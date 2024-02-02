54e7723d2b0d02861523eb1c5e0d815f1ad25a52
MiikkaJanesaro <122359891+MiikkaJanesaro@users.noreply.github.com>
2024-02-02 17:23:35 +0200

    valmis

---
+++ flatlist/Data.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, } from 'react-native';
+
+
+export const DATA = [
+    {
+      id: '1',
+      firstname: 'Elias',
+      lastname: 'Kiiskinen',
+    },
+    {
+      id: '2',
+      firstname: 'Miikka',
+      lastname: 'Jänesaro',
+    },
+    {
+      id: '3',
+      firstname: 'Alexander',
+      lastname: 'Harkimo',
+    },
+    {
+      id: '4',
+      firstname: 'Sophie',
+      lastname: 'Müller',
+    },
+    {
+      id: '5',
+      firstname: 'Antti',
+      lastname: 'Niinistö',
+    },
+    {
+      id: '6',
+      firstname: 'Isabel',
+      lastname: 'Silva',
+    },
+    {
+      id: '7',
+      firstname: 'Joon',
+      lastname: 'Lee',
+    },
+    {
+      id: '8',
+      firstname: 'Olivia',
+      lastname: 'Johnson',
+    },
+    {
+      id: '9',
+      firstname: 'Wei',
+      lastname: 'Wang',
+    },
+    {
+      id: '10',
+      firstname: 'Luis',
+      lastname: 'Martinez',
+    },
+    {
+      id: '11',
+      firstname: 'Emma',
+      lastname: 'Brown',
+    },
+    {
+      id: '12',
+      firstname: 'Amit',
+      lastname: 'Kumar',
+    },
+    {
+      id: '13',
+      firstname: 'Aliyah',
+      lastname: 'Muhammad',
+    },
+    {
+      id: '14',
+      firstname: 'Anders',
+      lastname: 'Andersen',
+    },
+    {
+      id: '15',
+      firstname: 'Huong',
+      lastname: 'Nguyen',
+    },
+  ];
